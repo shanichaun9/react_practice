7b0e94683135abefb946a5b06600d2244f2bfe3e
Shanichaun9 <sunnychaun9@gmail.com>
2024-09-13 16:17:20 +0530

    3-useEffect/useRef/useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState, useEffect, useRef } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-  let [counter, setCounter] = useState(0)
-  
-  const addValue = () => {
-    console.log('Value updated', {counter});
-    counter = counter + 1
-    setCounter(counter)
-  }
+  let [length, setLength] = useState(8)
+  let [numberAllowed, setNumberAllowed] = useState(false)
+  let [charAllowed, setCharAllowed] = useState(false)
+  let [password, setPassword] = useState('')
 
-  const removeCounter = () => {
-    setCounter(counter - 1)
-  }
+  const passwordRef = useRef(null)
+  const passwordGenerator = useCallback(
+    () => {
+      let pass = ''
+      let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+      if (numberAllowed) str += '0123456789'
+      if (charAllowed) str += '~!@#$%^&*()_+><":?/{}[]`'
+
+      for (let i = 1; i <= length; i++) {
+        let char = Math.floor(Math.random() * str.length + 1)
+        pass += str.charAt(char)
+      }
+      setPassword(pass)
+    },
+    [length, numberAllowed, charAllowed, setPassword],
+  )
+
+  const copyPassword = useCallback(
+    () => {
+      passwordRef.current.select()
+      // passwordRef.current?.setSelectionRange(0, 5)
+      window.navigator.clipboard.writeText(password)
+    },
+    [password],
+  )
+  
+  useEffect(() => {
+      passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+
+
   return (
     <>
-      <h1>Test</h1>
-      <h2>Counter Value : 4</h2>
-      <button onClick={addValue}>Add value {counter}</button>
-      <br />
-      <button disabled={counter < 1} onClick={removeCounter}>Remove Value {counter} </button>
-      <p>Footer: {counter} </p>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+        <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+        <div className='flex rounded-lg shadow mb-4 overflow-hidden'>
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='Password'
+            readOnly
+            ref={passwordRef}
+          />
+          <button onClick={copyPassword} className='bg-blue-700 text-white px-3 py-05 shrink-0'>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-2'>
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id='numberInput'
+              onChange={() => {
+                setNumberAllowed((prev) => !prev)
+              }}
+            />
+            <label htmlFor='numberInput'>Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id='numberInput'
+              onChange={() => {
+                setCharAllowed((prev) => !prev)
+              }}
+            />
+            <label htmlFor='numberInput'>Special Character</label>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
